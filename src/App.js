c1264670183af3d32dcd32536aa614c7922160ff
harv <harveyives@gmail.com>
2020-06-04 15:43:55 +0100

    testing hook again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing hook again again third time lucky...?
+          testing with deploy key
         </p>
         <a
           className="App-link"
