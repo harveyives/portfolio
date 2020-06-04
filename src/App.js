b2a7c956503f1f4da81e227d06f04f622372f26c
harv <harveyives@gmail.com>
2020-06-04 14:52:42 +0100

    testing hook again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing hook
+          Testing hook again
         </p>
         <a
           className="App-link"
