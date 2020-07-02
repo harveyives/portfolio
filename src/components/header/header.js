93ab11877f503724feea39eb253dd540745e3406
ives <harveyives@gmail.com>
2020-07-02 20:06:16 +0100

    removing gallery &&  some css files that get in the way

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
+import {Link} from 'gatsby';
 import posed from 'react-pose';
-import { Container } from './header.css';
-import Title from 'components/title';
 import Nav from 'components/header/nav';
 
 // Example of a component-specific page transition
@@ -24,13 +22,11 @@ const AnimatedContainer = posed.div({
 
 const Header = ({ title }) => (
   <AnimatedContainer>
-    <Container>
       <Link to="/">
-        <Title as="h1">{title}</Title>
+        {title}
       </Link>
 
       <Nav />
-    </Container>
   </AnimatedContainer>
 );
 
