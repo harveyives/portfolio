a6b84b26bb3f4ea7d0fd8bb474b408b2616583c8
ives <harveyives@gmail.com>
2020-08-23 15:38:06 +0200

    removing some random bloat from the starter + creating a markdown component because Chakra refuses to cooperate when trying to style headings from markdown?

---
+++ src/components/markdown/markdown.js
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+import {Box} from "@chakra-ui/core";
+import React from "react";
+
+const BoxWithHeadings = styled(Box)`
+  * {
+    white-space: pre;
+  }
+
+  h1 { font-size: 4rem; }
+  h2 { font-size: 3rem; }
+  h3 { font-size: 2.25rem; }
+  h4 { font-size: 1.875rem; }
+  h5 { font-size: 1.5rem; }
+  h6 { font-size: 1.25rem; }
+`
+
+export const Markdown = ({data}) => <BoxWithHeadings dangerouslySetInnerHTML={{
+  __html: data,
+}}/>;
